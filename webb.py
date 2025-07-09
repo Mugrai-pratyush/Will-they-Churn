88d7ef60a2a3ad5c330c2e0282a0d10f6effb823
Pratyush Mugrai <pratmugrai261105@gmail.com>
2025-07-09 16:20:02 +0530

    connectivity continues learning too

--- webb.py
+++ webb.py
@@ -4,4 +4,14 @@ from flask import Flask,request,render_template
 
 webapp=Flask(__name__)
 
-model = joblib.load("model/chmodel.pkl")
+model = joblib.load("model/chmodel.pkl") #loading the model 
+
+@webapp.route('/')#starting of the program
+#it should pull up the index.html right
+def start():
+    return render_template('index.html') 
+# When Predict is clicked we have to use the data input to predict the churn yay or nay
+def yay_nay():
+    return 
+    #will write the code for this latereak
+ 
